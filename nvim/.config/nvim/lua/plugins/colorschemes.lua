f715b4dcef700f0d5172b0c2d8a0a434a2eb5baa
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-05-22 10:17:37 -0500

    Removed some unused themes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,8 +1,5 @@
 return {
     { "ellisonleao/gruvbox.nvim" },
     { "sainnhe/sonokai" },
-    { "sainnhe/gruvbox-material" },
     { "Shatur/neovim-ayu" },
-    { "catppuccin/nvim", name = "catppuccin" },
-    { "navarasu/onedark.nvim" },
 }
