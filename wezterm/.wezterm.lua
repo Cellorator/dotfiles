f1e15d999dedc460d72e78d1f031476a7724b148
Cellorator <cjloreto3@gmail.com>
2023-05-21 22:22:23 -0500

    Changed font to DM Mono

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,16 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require ("wezterm")
 
 local config = {}
 
-config.font = wezterm.font 'Recursive Mono Linear Static'
---config.font = wezterm.font 'B612 Mono'
---config.font = wezterm.font 'Cascadia Code'
---config.font = wezterm.font 'Hermit'
+--config.font = wezterm.font "Recursive Mono Linear Static"
+--config.font = wezterm.font "B612 Mono"
+--config.font = wezterm.font "Cascadia Code"
+--config.font = wezterm.font "Hermit"
+--config.font = wezterm.font("B612 Mono")
+config.font = wezterm.font("DM Mono")
 
-config.harfbuzz_features = { 'ss01', 'ss02', 'ss03', 'ss04', 'ss05',}
+-- Recursive Mono features
+--config.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05",}
+config.harfbuzz_features = {"ss02", "ss03", "ss04",}
+-- 1234567890
 
-config.color_scheme = 'Gruvbox dark, medium (base16)'
---config.color_scheme = 'Ayu Mirage'
---config.color_scheme = 'ayu'
+config.color_scheme = "Gruvbox dark, medium (base16)"
+--config.color_scheme = "Ayu Mirage"
+--config.color_scheme = "ayu"
 
 return config
