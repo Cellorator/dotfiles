9f6e1277f5f0c170c4241576868030e0efcfa253
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-05-26 20:42:06 -0500

    Added some more mini.nvim modules

--- nvim/.config/nvim/lua/plugins/qol.lua
+++ nvim/.config/nvim/lua/plugins/qol.lua
@@ -1,15 +1,22 @@
 -- A bunch of quality-of-life plugins
 return {
     {
-        "lukas-reineke/indent-blankline.nvim",
-        ft = "*",
-    },
-    {
         "echasnovski/mini.nvim",
         config = function ()
+            require("mini.starter").setup()
+            require("mini.sessions").setup()
             require("mini.tabline").setup()
 
+            require("mini.jump").setup()
             require("mini.pairs").setup()
+            require("mini.trailspace").setup()
         end,
-    }
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function ()
+            require("indent_blankline").setup()
+        end,
+        ft = "*",
+    },
 }
