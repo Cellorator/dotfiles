a33c01aa1275b5de05ccd1b22b5e3023a47de4e9
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-05-26 19:51:53 -0500

    Reinstalled indent-blankline and fixed autocomplete again

--- nvim/.config/nvim/lua/plugins/qol.lua
+++ nvim/.config/nvim/lua/plugins/qol.lua
@@ -1,11 +1,15 @@
 -- A bunch of quality-of-life plugins
 return {
     {
+        "lukas-reineke/indent-blankline.nvim",
+        ft = "*",
+    },
+    {
         "echasnovski/mini.nvim",
         config = function ()
             require("mini.tabline").setup()
 
             require("mini.pairs").setup()
-        end
+        end,
     }
 }
