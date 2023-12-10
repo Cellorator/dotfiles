233bcd6b032c83dc6be23e2f56dc3c29d6090cee
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-10 14:26:19 -0600

    Added some yadm abbreviations and modified some

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,16 +7,14 @@ fish_add_path ~/.cargo/bin
 
 starship init fish | source
 zoxide init fish | source
-
-set -gx EDITOR nvim
 set -x BAT_THEME ansi
 
 # Useful abbreviations and aliases
 abbr -a vim nvim
 abbr -a cat bat
-abbr -a ls exa --icons
-abbr -a la exa -la --icons
-abbr -a tree exa -Ta --icons
+abbr -a ls eza --icons
+abbr -a la eza -la --icons
+abbr -a tree eza -Ta --icons
 abbr -a cd z
 abbr -a stow stow --dotfiles
 
@@ -29,13 +27,18 @@ alias bruh=thefuck
 abbr -a gi git init
 abbr -a gc git clone
 abbr -a ga git add
-abbr -a gc git commit
+abbr -a --set-cursor gc git commit -m \"%\"
 abbr -a gs git status
 abbr -a gl git log
 abbr -a gp git push
 
+# Yadm abbreviations
+abbr -a ya yadm add
+abbr -a --set-cursor yc yadm commit -m \"%\"
+abbr -a ys yadm status
+abbr -a yp yadm push
+
 alias backup-prism="rsync -azvP --delete /home/admin/.local/share/PrismLauncher/instances/ /run/media/admin/main-32gb/MinecraftMods/InstanceBackups/"
 alias restore-prism="rsync -avzP --delete '/run/media/admin/main-32gb/MinecraftMods/InstanceBackups/' /home/admin/.local/share/PrismLauncher/instances/"
-#alias shifttime="~/scripts/shifttime"
 
 set fish_greeting
