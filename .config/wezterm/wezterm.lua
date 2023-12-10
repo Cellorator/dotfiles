1ffe2169fc9ecb8e678cd91a6733632b0ced7a6a
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-10 14:32:25 -0600

    Disabled tabbars and removed related keybinds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,31 +2,9 @@ local wezterm = require ("wezterm")
 
 local config = {}
 
+config.enable_tab_bar = false
+
 config.font = wezterm.font("DMMono Nerd Font")
 config.color_scheme = "Gruvbox dark, medium (base16)"
-
-config.keys = {
-    {
-        key = "Enter",
-        mods = "CTRL",
-        action = wezterm.action.SpawnTab("CurrentPaneDomain")
-    },
-    {
-        key = "w",
-        mods = "CTRL",
-        action = wezterm.action.CloseCurrentTab({confirm = false})
-    },
-    {
-        key = "h",
-        mods = "CTRL",
-        action = wezterm.action.ActivateTabRelative(-1)
-    },
-    {
-        key = "l",
-        mods = "CTRL",
-        action = wezterm.action.ActivateTabRelative(1)
-    },
-
-}
 
 return config
