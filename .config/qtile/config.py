4317143a01298b064c3ab3852280f0bf1e780b72
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-11 17:26:44 -0600

    Configured layouts and changed some binds

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -9,6 +9,7 @@ def autostart_once():
 mod = "mod4"
 
 keys = [
+    Key([mod], "Tab", lazy.screen.toggle_group(), desc="Move window focus to other window"),
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
@@ -16,7 +17,7 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
@@ -29,27 +30,24 @@ keys = [
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "control"], "i", lazy.layout.grow()),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn("wezterm"), desc="Launch terminal"),
-    Key([mod], "b", lazy.spawn("firefox"), desc="Spawn browser"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
+    
     Key([mod], "f", lazy.window.toggle_floating()),
     Key([mod], "m", lazy.window.toggle_maximize(), desc="Toggle maximize"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "Return", lazy.spawn("wezterm"), desc="Launch terminal"),
+    Key([mod], "b", lazy.spawn("firefox"), desc="Spawn browser"),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -79,24 +77,17 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2),
-    #layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    #layout.Stack(num_stacks=2),
-    #layout.Bsp(),
-    #layout.Matrix(),
-    layout.MonadTall(),
-    #layout.MonadWide(),
-    #layout.RatioTile(),
-    #layout.Tile(), #layout.TreeTab(),
-    #layout.VerticalTile(),
-    #layout.Zoomy(),
+    layout.Columns(
+        border_focus="#83a598",
+        border_focus_stack="#689d6a",
+        border_normal="#000000",
+        border_normal_stack="#000000",
+        border_width=1),
 ]
 
 widget_defaults = dict(
     font="DM Mono Nerd Font",
     fontsize=14,
-    padding=2,
 )
 extension_defaults = widget_defaults.copy()
 
