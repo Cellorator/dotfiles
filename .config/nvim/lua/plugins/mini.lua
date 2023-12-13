8e298792fef6807548308f1bc5c17168e6410664
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-12 20:29:04 -0600

    Enabled mini.surround

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,18 +1,18 @@
 return {
     {
-        "echasnovski/mini.nvim",
+        "echasnovski/mini.nim",
         config = function ()
             require("mini.starter").setup()
             require("mini.sessions").setup()
-
             require("mini.jump").setup()
             require("mini.pairs").setup()
+            require("mini.surround").setup()
             require("mini.trailspace").setup()
             require("mini.comment").setup({
                 mappings = {
                     comment = '<leader>/',
                     comment_line = '<leader>/',
-                    comment_visual = '<leader>/',
+                    comment_isual = '<leader>/',
                     textobject = '',
                 },
             })
