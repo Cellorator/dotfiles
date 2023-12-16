8526add4aab0d45a7016e14e085f4e5addd57e64
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-15 22:57:48 -0600

    Removed mini.jump

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,6 @@ return {
     {
         "echasnovski/mini.nvim",
         config = function ()
-            require("mini.jump").setup()
             require("mini.pairs").setup()
             require("mini.trailspace").setup()
             require("mini.comment").setup()
