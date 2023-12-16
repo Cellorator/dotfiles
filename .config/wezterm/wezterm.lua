59c9f5d73ec93620279c2a7b530697a1a9bcae48
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-15 21:42:36 -0600

    Made text color in cursor black for visibility

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,11 @@ local config = {}
 config.enable_tab_bar = false
 
 config.font = wezterm.font("DM Mono")
+
+-- Colorscheme
 config.color_scheme = "Gruvbox Material (Gogh)"
+config.colors = {
+    cursor_fg = "black"
+}
 
 return config
