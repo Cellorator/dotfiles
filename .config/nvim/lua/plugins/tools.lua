37328622d1c2170b0685b004484dc3ca62021509
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-15 22:52:43 -0600

    Installed flash.nvim

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,9 +1,43 @@
 return {
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function ()
-        local map = vim.keymap.set
-        map("n", "<leader>ff", "<cmd>FzfLua files<CR>")
-        map("n", "<leader>fr", "<cmd>FzfLua buffers<CR>")
-    end
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function ()
+            local map = vim.keymap.set
+            map("n", "<leader>ff", "<cmd>FzfLua files<CR>")
+            map("n", "<leader>fr", "<cmd>FzfLua buffers<CR>")
+        end
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "s", mode = { "n", "x", "o" },
+                function() require("flash").jump() end,
+                desc = "Flash"
+            },
+            {
+                "S", mode = { "n", "x", "o" },
+                function() require("flash").treesitter() end,
+                desc = "Flash Treesitter"
+            },
+            {
+                "r", mode = "o",
+                function() require("flash").remote() end,
+                desc = "Remote Flash"
+            },
+            {
+                "R", mode = { "o", "x" },
+                function() require("flash").treesitter_search() end,
+                desc = "Treesitter Search"
+            },
+            {
+                "<c-s>", mode = { "c" },
+                function() require("flash").toggle() end,
+                desc = "Toggle Flash Search"
+            },
+        },
+    }
 }
