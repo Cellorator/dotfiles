d8e8d80446a6043f95210184b4145c3501ef4214
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-15 22:54:13 -0600

    Removed mini.surround

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,7 +4,6 @@ return {
         config = function ()
             require("mini.jump").setup()
             require("mini.pairs").setup()
-            require("mini.surround").setup()
             require("mini.trailspace").setup()
             require("mini.comment").setup()
         end,
