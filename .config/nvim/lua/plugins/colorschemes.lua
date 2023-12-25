8b13ac7d6762875efcd70ae857cd865b24b298a5
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-25 10:48:43 -0600

    Changed gruvbox-material foreground theme to 'mix'

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -10,6 +10,7 @@ return {
         "sainnhe/gruvbox-material",
         priority = 1000,
         config = function ()
+			vim.g.gruvbox_material_foreground = "mix"
             vim.cmd.colorscheme("gruvbox-material")
         end
     },
