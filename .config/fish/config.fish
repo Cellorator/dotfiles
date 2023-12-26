df730754f361eca6b61aa90f4510590b12f2ac23
Cellorator <66387348+Cellorator@users.noreply.github.com>
2023-12-26 15:20:35 -0600

    Changed eza abbreviations to aliases and add abbreviations for git diff and yadm diff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,10 +12,11 @@ set -x BAT_THEME ansi
 # Useful abbreviations and aliases
 abbr -a vim nvim
 abbr -a cat bat
-abbr -a ls eza --icons
-abbr -a la eza -la --icons
-abbr -a tree eza -Ta --icons
 abbr -a cd z
+
+alias ls="eza -l --icons"
+alias la="eza -la --icons"
+alias tree="eza -ta --icons"
 
 # Git abbreviations
 abbr -a gi git init
@@ -23,6 +24,7 @@ abbr -a gc git clone
 abbr -a ga git add
 abbr -a --set-cursor gc git commit -m \"%\"
 abbr -a gs git status
+abbr -a gd git diff
 abbr -a gl git log
 abbr -a gp git push
 
@@ -30,6 +32,7 @@ abbr -a gp git push
 abbr -a ya yadm add
 abbr -a --set-cursor yc yadm commit -m \"%\"
 abbr -a ys yadm status
+abbr -a yd yadm diff
 abbr -a yp yadm push
 
 alias backup-prism="rsync -azvP --delete /home/admin/.local/share/PrismLauncher/instances/ /run/media/admin/main-32gb/MinecraftMods/InstanceBackups/"
