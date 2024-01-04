7e7f89299b016647817eddd2bc5a742cec3c653e
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-01-03 22:49:32 -0600

    Removed border

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,12 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.enable_tab_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0
+}
 
 config.font = wezterm.font("DM Mono")
 
