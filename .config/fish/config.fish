19f7caf1ca146dd4237c877aed997104a696b46b
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-05-11 14:37:20 -0500

    Changed zoxide alias back to and abbreviation and move starship and zoxide init at bottom

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,23 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting
+
 fish_add_path ~/bin
 fish_add_path ~/.cargo/bin
 
 set -Ux EDITOR nvim
 set -Ux QT_QPA_PLATFORMTHEME qt5ct
 
-starship init fish | source
-zoxide init fish | source
-
 # Useful abbreviations and aliases
 abbr -a cl clear
+abbr -a cd z
 
 alias ls="eza -1 --icons"
 alias la="eza -la --icons"
 alias tree="eza -Ta --icons"
 alias cat="bat"
-alias cd="z"
 alias cat="bat"
 alias vim="nvim"
 
@@ -39,4 +38,6 @@ abbr -a ys yadm status
 abbr -a yd yadm diff
 abbr -a yp yadm push
 
-set fish_greeting
+
+starship init fish | source
+zoxide init fish | source
