e3910c2c7a24d05b3afecbeb2953e1b66c9a56c5
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-05-12 16:50:02 -0500

    Removed theme option in lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -4,7 +4,6 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			options = {
-				theme = "gruvbox_dark",
 				section_separators = { left = "", right = ""},
 				component_separators = { left = "", right = "" },
 			},
