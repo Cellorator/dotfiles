ed95e116a526fca4685871f40daf83735ef9938a
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-05-12 15:43:49 -0500

    Removed flash.nvim for now

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -9,26 +9,26 @@ return {
             map("n", "<leader>dd", "<cmd>FzfLua dap_commands<CR>")
         end
     },
-    {
-        "folke/flash.nvim",
-        event = "VeryLazy",
-        opts = {},
-        keys = {
-            {
-                "s", mode = { "n", "x", "o" },
-                function() require("flash").jump() end,
-                desc = "Flash"
-            },
-            {
-                "S", mode = { "n", "x", "o" },
-                function() require("flash").treesitter() end,
-                desc = "Flash Treesitter"
-            },
-            {
-                "<c-s>", mode = { "c" },
-                function() require("flash").toggle() end,
-                desc = "Toggle Flash Search"
-            },
-        },
-    }
+    -- {
+    --     "folke/flash.nvim",
+    --     event = "VeryLazy",
+    --     opts = {},
+    --     keys = {
+    --         {
+    --             "s", mode = { "n", "x", "o" },
+    --             function() require("flash").jump() end,
+    --             desc = "Flash"
+    --         },
+    --         {
+    --             "S", mode = { "n", "x", "o" },
+    --             function() require("flash").treesitter() end,
+    --             desc = "Flash Treesitter"
+    --         },
+    --         {
+    --             "<c-s>", mode = { "c" },
+    --             function() require("flash").toggle() end,
+    --             desc = "Toggle Flash Search"
+    --         },
+    --     },
+    -- }
 }
