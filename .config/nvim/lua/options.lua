e037bcc177ef5035d88f6121e21e47491ce248a3
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-06-16 22:40:12 -0500

    Updated nvim plugins

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,14 +6,15 @@ opt.relativenumber = true  -- Show relative line numbers
 opt.signcolumn = "number"  -- Put gutter symbols in line number column
 
 -- Behaviour
--- opt.expandtab = true
+opt.expandtab = true
 opt.tabstop = 4  -- Length of a tab
-opt.softtabstop = 4  -- Amount of spaces for tab
 opt.shiftwidth = 4  -- Size of indentation with > and < commands
+opt.softtabstop = 4  -- Amount of spaces for tab
+opt.smarttab = true
 opt.scrolloff = 8  -- Amount of lines before top and bottom while scrolling
 
 -- Search
-opt.ignorecase = true  -- Ignore case when searchin
+opt.ignorecase = true  -- Ignore case when searching
 opt.smartcase = true  -- Don't ignore case when search has upper case letters
 opt.hlsearch = false  -- Stop highlighting when search is done
 
