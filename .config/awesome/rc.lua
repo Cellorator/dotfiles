d22daaf3555a563611b8c4741af3437963617d17
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-02 11:36:12 -0500

    Moved layoutbox widget creation to separate fiile

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -162,27 +162,10 @@ awful.screen.connect_for_each_screen(function(s)
 
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    -- s.mylayoutbox = awful.widget.layoutbox(s)
-    s.layoutbox = wibox.widget({
-        {
-            awful.widget.layoutbox(s),
-            widget = wibox.container.margin,
-            left = 8,
-            right = 8
-        },
-        widget = wibox.container.background,
-        bg = beautiful.bg_focus
-    })
-    s.layoutbox:buttons(gears.table.join(
-        awful.button({ }, 1, function () awful.layout.inc( 1) end),
-        awful.button({ }, 3, function () awful.layout.inc(-1) end),
-        awful.button({ }, 4, function () awful.layout.inc( 1) end),
-        awful.button({ }, 5, function () awful.layout.inc(-1) end)))
 
+    -- Create widgets
+    s.layoutbox = require("widgets.layoutbox").create(s)
     s.taglist = require("widgets.taglist").create(s)
-
     -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist({
         screen  = s,
