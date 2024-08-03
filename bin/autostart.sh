27f636d84ad67bae8e503534d242c3a4e483a5fb
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-02 21:50:51 -0500

    Added alt-tabbing

---
+++ bin/autostart.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Only run process if not running already
+run() {
+    if ! pgrep -f "$1" ;
+    then
+        "$@"&
+    fi
+}
+run "picom" -b
+run "/usr/lib/xfce-polkit/xfce-polkit"
+run "dunst"
+run "udiskie" --tray
+run "blueman-applet"
+run "nm-applet" --no-agent
+run "gwe" --hide-window
+run "easyeffects" --gapplication-service
+
+# Kill and run alttab, so settings apply on WM reload
+pkill alttab
+alttab -w 1 -d 2 -t 192x224 -i 192x96 -fg "#c5c9c5" -bg "#181616" -frame "#8ba4b0" -font "xft:Hurmit Nerd Font-11" &
+
+# Disable mouse acceleration and set key repeat rate
+xinput set-prop 9 'libinput Accel Profile Enabled' 0 1
+xset r rate 300
+
