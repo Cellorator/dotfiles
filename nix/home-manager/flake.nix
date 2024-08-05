268d1f54cc8dc5a70175907aa4f4133d568c00ea
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-05 11:12:31 -0500

    WHHHHHHYY

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -2,7 +2,7 @@
 	description = "Home Manager configuration";
 
 	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-unstable";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
 		home-manager = {
 			url = "github:nix-community/home-manager";
