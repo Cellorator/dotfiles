1dd5ac9a3fc6bd61dabfa1bf2f6c593e527183f4
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-05 14:32:31 -0500

    Cool

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -14,15 +14,23 @@ in {
 
 		packages = with pkgs; [
 			fish
+            starship
+            pfetch
 			bat
 			zoxide
             eza
+            fzf
 
 			wezterm
 			neovim
 
+            xclip
             rofi
             picom
+
+            (nerdfonts.override { fonts = [ "Hermit" ]; })
+
+            clang
 		];
 
 		preferXdgDirectories = true;
@@ -30,7 +38,7 @@ in {
 		file = {
 			"bin" = configLink "bin";
 			".config/nvim" = configLink ".config/nvim";
-			# ".config/fish" = configLink ".config/fish";
+			".config/fish" = configLink ".config/fish";
 			".config/wezterm" = configLink ".config/wezterm";
 			".config/awesome" = configLink ".config/awesome";
 			".config/picom" = configLink ".config/picom";
@@ -39,7 +47,8 @@ in {
 	};
 
 	programs = {
-        fish.enable = true;
 		home-manager.enable = true;
 	};
+
+    fonts.fontconfig.enable = true;
 }
