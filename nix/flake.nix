e22fc4889f345a92381d0356b634ce1d0b01aa7f
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-06 14:36:40 -0500

    Fixed weird indentation

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,26 +1,26 @@
 {
-  description = "My Home Manager configuration";
+    description = "My Home Manager configuration";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      homeConfigurations = {
-        admin = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home-manager/home.nix ];
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
-      };
+    };
+
+    outputs = { nixpkgs, home-manager, ... }:
+        let
+            lib = nixpkgs.lib;
+            system = "x86_64-linux";
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            homeConfigurations = {
+                admin = home-manager.lib.homeManagerConfiguration {
+                    inherit pkgs;
+                    modules = [ ./home-manager/home.nix ];
+                };
+            };
     };
 }
