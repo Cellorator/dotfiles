f6295c0613bb560affd535bf6e33dc6990ae341f
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-09 15:33:14 -0500

    Made nixos to use latest linux kernel

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -21,6 +21,8 @@
     };
     boot.loader.efi.canTouchEfiVariables = true;
 
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
     networking.hostName = hostname; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
