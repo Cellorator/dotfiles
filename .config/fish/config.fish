db442ccdb03f43c658d703ac6c04f18a12552749
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-11 14:19:56 -0500

    Enabled eza filetype sorting so folders are at top

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,10 +19,9 @@ abbr -a cd z
 abbr vim nvim
 abbr vi nvim
 
-alias ls="eza -1 --icons"
-alias la="eza -la --icons"
-alias tree="eza -Ta --icons"
-alias cat="bat"
+alias ls="eza -1 --sort=type --icons"
+alias la="eza -la --sort=type --icons"
+alias tree="eza -Ta --sort=type --icons"
 alias cat="bat"
 
 # Zellij abbreviations
