b40a5739d758684a117bc7398bee5769d0b700ec
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-14 18:31:28 -0500

    Moved nixos files to right places

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,27 +11,27 @@
     };
 
     outputs = { nixpkgs, home-manager, ... }@inputs:
-        let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-            };
-        in {
-            nixosConfigurations = {
-               desktop = nixpkgs.lib.nixosSystem {
-                    inherit system;
-                    specialArgs = inputs;
-                    modules = [ ./hosts/desktop ];
-               };
-            };
-
-            homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-                inherit pkgs;
-                extraSpecialArgs = inputs // { inherit username; };
-                modules = [
-                    ./home-manager/home.nix
-                ];
-            };
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
         };
+    in {
+        nixosConfigurations = {
+           desktop = nixpkgs.lib.nixosSystem {
+                inherit system;
+                specialArgs = inputs;
+                modules = [ ./hosts/desktop ];
+           };
+        };
+
+        homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = inputs // { inherit username; };
+            modules = [
+                ./home-manager/home.nix
+            ];
+        };
+    };
 }
