2333c1cb0a41c90a875e98439d8bd57dbefc8041
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-16 15:47:04 -0500

    Removed config. in gtk-theme module

--- nix/modules/home/extras/gtk-theme.nix
+++ nix/modules/home/extras/gtk-theme.nix
@@ -1,12 +1,12 @@
 # Sets gtk theming options
 { config, utils, ... }:
 {
-    config.home.file = utils.linkToHome (utils.prepend ".local/share/" [
+    home.file = utils.linkToHome (utils.prepend ".local/share/" [
         "themes/KanagawaDragon"
         "icons/KanagawaDragon"
     ]);
 
-    config.gtk = {
+    gtk = {
         enable = true;
         font.name = "Hurmit Nerd Font";
         theme.name = "KanagawaDragon";
