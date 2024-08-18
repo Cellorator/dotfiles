7459ed37aebfd64fbc5542d1cab013e8b038a61e
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-18 17:39:54 -0500

    Changed awesome terminal to kitty

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -55,7 +55,7 @@ beautiful.init("/home/admin/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 
-terminal = "wezterm"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
