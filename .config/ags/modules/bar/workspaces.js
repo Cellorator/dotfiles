23c06a8758766717dc6e2da63cb2aed06a388e53
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-22 21:55:36 -0500

    Made workspaces with clients have differenct class names to theme

--- .config/ags/modules/bar/workspaces.js
+++ .config/ags/modules/bar/workspaces.js
@@ -1,30 +1,26 @@
 const hyprland = await Service.import("hyprland")
 
 export default function Workspaces() {
+    const activeId = hyprland.active.workspace.bind("id").as(Number)
+    // Get array of ids of workspaces with at least one client
+    const idsWithClients = hyprland.bind("workspaces").as(workspaces => {
+        return workspaces.filter(ws => ws.windows !== 0).map(ws => ws.id)
+    })
+
     const workspaces = [...Array(9).keys()]
         .map(id => id + 1)
         .map(id => Widget.Button({
             child: Widget.Label(`${id}`),
             on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-            className: currentStatus(id),
+            className: Utils.merge([activeId, idsWithClients], (activeId, idsWithClients) => {
+                if (activeId === id) return "active"
+                if (idsWithClients.includes(id)) return "hasClients"
+                return ""
+            })
         }))
-
-    hyprland.connect("client-added", address => print(address))
 
     return Widget.Box({
         className: "workspaces",
         children: workspaces,
     })
-}
-
-function currentStatus(id) {
-    const activeId = hyprland.active.workspace.bind("id").as(String)
-
-    if (!hasAClient(id)) return ""
-    if (id === activeId) return "active"
-    return "hasClient"
-}
-
-function hasAClient(id) {
-    return hyprland.workspaces.filter(ws => ws.id === id).length != 0
 }
