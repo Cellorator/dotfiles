00a891678528c9760efd400a8c7768aa2d4433f9
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-23 00:33:48 -0500

    im so confused

--- .config/ags/modules/notifications/main.js
+++ .config/ags/modules/notifications/main.js
@@ -1,22 +1,45 @@
 const notifications = await Service.import("notifications")
 
 export default function Notifications() {
+    const list = Widget.Box({
+        vertical: true,
+        homogeneous: false,
+        spacing: 6,
+        children: [Widget.Label("sdfhjs")]
+    })
+
+    list.hook(notifications, (_, id) => {
+        const n = notifications.getNotification(id)
+        if (n)
+            list.children = [Notification(n), ...list.children]
+    }, 'notified')
+
+    // list.hook(notifications, (_, id) => {
+    //     list.children.find(n => )
+    // })
+
     return Widget.Window({
         name: 'notifications',
         className: 'notifications',
+        css: "background-color: transparent",
         anchor: ['right', 'top', 'bottom'],
         margins: [35, 6, 0, 0],
         exclusivity: "ignore",
-        child: Widget.Box({
-            vertical: true,
-            homogeneous: false,
-            spacing: 6,
-            // children: notifications.bind('popups').as(notifs => notifs.map(Notification))
-            children: [ Widget.Label('hello') ]
-        })
+        child: list
     })
 }
 
-function Notification() {
-    return Widget.Label('hello')
+function Notification(n) {
+    print(n.summary)
+    const title = Widget.Label({
+        label: n.summary
+    })
+
+    return Widget.EventBox({
+        child: Widget.Box({
+            children: [
+                title,
+            ]
+        }),
+    })
 }
