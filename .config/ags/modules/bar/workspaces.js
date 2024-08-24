74fe33600961e9e559cec06dd5e873d00f683f28
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-24 01:27:59 -0500

    Beginnings of dynamic separators

--- .config/ags/modules/bar/workspaces.js
+++ .config/ags/modules/bar/workspaces.js
@@ -1,4 +1,4 @@
-import { ForwardSlashSeparator } from "../widgets/separator.js"
+import { ForwardSlashSeparator, RightHardCircleSeparator } from "../widgets/separator.js"
 const hyprland = await Service.import("hyprland")
 
 export default function Workspaces() {
@@ -18,16 +18,34 @@ const idsWithClients = hyprland.bind("workspaces").as(workspaces => {
     return workspaces.filter(ws => ws.windows !== 0).map(ws => ws.id)
 })
 function Workspace(id) {
+    const status = Utils.merge([activeId, idsWithClients], (activeId, idsWithClients) => {
+        if (activeId === id) return 'active'
+        if (idsWithClients.includes(id)) return 'hasClients'
+        return ''
+    })
+
+    const content = Widget.Box({
+        children: status.as(s => {
+            const arr = [
+                Widget.Label({
+                    label: `${id}`,
+                    class_name: 'label'
+                })
+            ]
+            if (s == 'active') {
+                arr.push(RightHardCircleSeparator())
+            } else {
+                arr.push(ForwardSlashSeparator())
+            }
+            return arr
+        })
+    })
+
     return Widget.Button({
         name: `workspace${id}`,
-        className: Utils.merge([activeId, idsWithClients], (activeId, idsWithClients) => {
-            if (activeId === id) return "active"
-            if (idsWithClients.includes(id)) return "hasClients"
-            return ""
-        }),
+        class_name: status,
+        attribute: status.as(i => `${i}`),
         on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-        child: Widget.Box({
-            children: [Widget.Label(`${id}`), ForwardSlashSeparator()]
-        })
+        child: content,
     })
 }
--- .config/ags/modules/widgets/separator.js
+++ .config/ags/modules/widgets/separator.js
@@ -1,8 +1,16 @@
-export { ForwardSlashSeparator }
+export { ForwardSlashSeparator, RightHardCircleSeparator }
 
 function ForwardSlashSeparator() {
+    return Separator('')
+}
+
+function RightHardCircleSeparator() {
+    return Separator('')
+}
+
+function Separator(str, class_name) {
     return Widget.Label({
-        label: '',
-        className: "separator forward-slash"
+        label: str,
+        class_name: `separator`
     })
 }
