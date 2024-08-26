baf691da5cddf78a17c0f1d8e430b7ff4a189314
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-08-26 15:52:51 -0500

    Cool

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -10,8 +10,8 @@ App.config({
 })
 
 Utils.timeout(100, () => Utils.notify({
-    summary: "cool",
-    body: "AGGGFDDF",
+    summary: "cooooooooooooooool",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     actions: {
         "COOL AFAIN": () => print("pressed"),
     },
