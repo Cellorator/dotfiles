ea0fb0125ee3e11820d53a60e1aefee93af0c0cc
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-09-06 19:22:25 -0500

    Removed flash.nvim and toggleterm

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -9,34 +9,4 @@ return {
             map("n", "<leader>dd", "<cmd>FzfLua dap_commands<CR>")
         end
     },
-	{
-		"akinsho/toggleterm.nvim",
-		config = function ()
-			require("toggleterm").setup()
-            local map = vim.keymap.set
-			map({"n", "t"}, "<leader>tt", "<cmd>ToggleTerm<CR>")
-		end
-	},
-    -- {
-    --     "folke/flash.nvim",
-    --     event = "VeryLazy",
-    --     opts = {},
-    --     keys = {
-    --         {
-    --             "s", mode = { "n", "x", "o" },
-    --             function() require("flash").jump() end,
-    --             desc = "Flash"
-    --         },
-    --         {
-    --             "S", mode = { "n", "x", "o" },
-    --             function() require("flash").treesitter() end,
-    --             desc = "Flash Treesitter"
-    --         },
-    --         {
-    --             "<c-s>", mode = { "c" },
-    --             function() require("flash").toggle() end,
-    --             desc = "Toggle Flash Search"
-    --         },
-    --     },
-    -- }
 }
