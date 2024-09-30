9175db579e2d77752d4073d0d75fb40c2bcb3ca0
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-09-30 11:37:42 -0500

    Installed nabla.nvim again and nvim-ufo and nvim-origami

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -10,6 +10,30 @@ return {
         end
     },
     {
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async" },
+        config = function ()
+            vim.o.foldcolumn = "0" -- '0' is not bad
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            -- Using ufo provider need remap `zR` and `zM`.
+            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+            require("ufo").setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return {"treesitter", "indent" }
+                end
+            })
+        end
+    },
+    {
+        "chrisgrieser/nvim-origami",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
         "epwalsh/obsidian.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = {
@@ -25,5 +49,11 @@ return {
             },
             ui = { enable = false },
         }
+    },
+    {
+        "jbyuki/nabla.nvim",
+        config = function ()
+            vim.keymap.set("n", "<leader>p", ":lua require('nabla').popup()<CR>")
+        end
     }
 }
