373cb760d4207258da400ed2167ce3e9dd76aa52
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-09-30 20:00:49 -0500

    Did some emacs configuration

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,2 +1,40 @@
 ;; Don't show splash screen
 (setq inhibit-startup-message t)
+
+(tool-bar-mode -1) ; Disable tool bar
+(scroll-bar-mode -1) ; Disable scroll bar
+
+(column-number-mode) ; Display column number on mode bar
+(global-display-line-numbers-mode 1) ; Display line numbers
+
+
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package kanagawa-themes
+    :ensure t
+    :config
+    (load-theme 'kanagawa-dragon t))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(kanagawa-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
