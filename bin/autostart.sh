f924c2f0d791aae6573bfed40e7d793f130908fa
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-10-01 22:52:19 -0500

    Removed a bunch of autostart stuff

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -9,13 +9,10 @@ run() {
 }
 
 run "picom" -b
-run "/usr/lib/xfce-polkit/xfce-polkit"
-run "dunst"
 run "udiskie" --tray
 run "blueman-applet"
 run "nm-applet" --no-agent
-run "gwe" --hide-window
-run "easyeffects" --gapplication-service
+run "snixembed"
 
 # Kill and run alttab, so settings apply on WM reload
 pkill alttab
@@ -25,4 +22,3 @@ alttab -w 1 -d 2 -t 192x224 -i 192x96 -fg "#c5c9c5" -bg "#181616" -frame "#8ba4b
 xinput set-prop 9 'libinput Accel Profile Enabled' 0 1
 xset r rate 300
 
-run "snixembed"
