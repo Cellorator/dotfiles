cce4833d550e56475d4eeafa05f2104c498d3e9d
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-10-04 21:46:27 -0500

    Switched to elpaca package manager for emacs

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,2 +1,4 @@
+(setq package-enable-at-startup nil)
+
 (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin" (getenv "PATH")))
 (push "/data/data/com.termux/files/usr/bin" exec-path)
