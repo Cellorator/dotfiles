03dc1c8ae427f5aa7df578f0f9665290489b2636
Cellorator <66387348+Cellorator@users.noreply.github.com>
2024-12-17 11:07:30 -0600

    Emacs added some stuff to init.el

--- emacs/init.el
+++ emacs/init.el
@@ -554,3 +554,18 @@
 ;;   (if (eq major-mode 'pdf-view-mode)
 ;;       (org-noter-sync-current-note)
 ;;     (org-noter)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-export-use-babel 'inline-only nil nil "Customized with use-package org")
+ '(org-preview-latex-default-process 'dvisvgm nil nil "Customized with use-package org")
+ '(package-selected-packages
+   '(org-noter citar-embark citar-org-roam citar denote org-roam-ui org-roam org-auto-tangle olivetti org-fragtog org-appear org-modern kanagawa-themes restart-emacs evil-commentary smartparens yasnippet which-key vertico undo-fu-session undo-fu treesit-auto pdf-tools orderless marginalia magit lsp-mode general evil-org evil-collection embark-consult corfu cape)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
