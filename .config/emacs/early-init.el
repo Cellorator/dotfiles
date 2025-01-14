d93186149a3a3f8f443e10aabdfb64aaa97ea2f9
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-01-13 21:27:01 -0600

    Removed duplicate folders in deprecated .config folder

--- .config/emacs/early-init.el
+++
@@ -1,4 +0,0 @@
-(when (eq system-type 'android)
-  (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
-			 (getenv "PATH")))
-  (push "/data/data/com.termux/files/usr/bin" exec-path))
