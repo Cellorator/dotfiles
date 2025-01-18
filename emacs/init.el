a9e62e0d7873aea67721f14f7c859b0e34683fda
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-01-18 16:57:26 -0600

    Removed embark from emacs

--- emacs/init.el
+++ emacs/init.el
@@ -215,18 +215,6 @@
 ;; (use-package python-mode :ensure t)
 ;; (use-package lua-mode :ensure t)
 
-(use-package embark
-  :general
-  (<leader>
-    :states '(normal visual)
-    "RET" 'embark-act)
-  :ensure t)
-(use-package embark-consult
-  :ensure t ; only need to install it, embark loads it after consult if found
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode)
-  :ensure t)
-
 ;; Annotations in completion UI
 (use-package marginalia
   :init (marginalia-mode)
