440f63330de1cad6405ca155a3ed5e0aecfe866d
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-01-22 19:42:02 -0600

    Enabled emacs-lsp-booster stuff

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -2,3 +2,5 @@
   (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
 			 (getenv "PATH")))
   (push "/data/data/com.termux/files/usr/bin" exec-path))
+
+(setenv "LSP_USE_PLISTS" "true")
--- emacs/init.el
+++ emacs/init.el
@@ -213,6 +213,37 @@
    (python-ts-mode . lsp-deferred))
   :ensure t)
 
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+            (setcar orig-result command-from-exec-path))
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
@@ -367,6 +398,7 @@
   (:keymaps 'override (general-nmap "RET" 'org-open-at-point)))
 
 (use-package org
+  :custom (org-startup-with-latex-preview t)
   :config
   ;; Resize Org headings
   ;; (dolist (face '((org-level-1 . 1.5)
@@ -389,7 +421,7 @@
   :custom
   (org-modern-star 'replace)
   (org-modern-keyword nil)
-  :hook org-mode
+  :hook (org-mode)
   :ensure t)
 
 ;; Make stuff dissapear and stuff
@@ -402,8 +434,7 @@
 
 ;; Preview latex in editor
 (use-package org-fragtog
-  :custom (org-startup-with-latex-preview t)
-  :hook org-mode
+  :hook (org-mode org-roam-mode)
   :after org
   :ensure t)
 
@@ -608,3 +639,18 @@
 ;;   (if (eq major-mode 'pdf-view-mode)
 ;;       (org-noter-sync-current-note)
 ;;     (org-noter)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-export-use-babel 'inline-only nil nil "Customized with use-package org")
+ '(org-preview-latex-default-process 'dvisvgm nil nil "Customized with use-package org")
+ '(package-selected-packages
+   '(yasnippet which-key vertico undo-fu-session undo-fu treesit-auto telephone-line smartparens restart-emacs pdf-tools org-roam-ui org-noter org-modern org-fragtog org-auto-tangle org-appear orderless olivetti marginalia magit lua-mode kanagawa-themes general frames-only-mode evil-org evil-commentary evil-collection embark-consult eat dtrt-indent doom-themes denote corfu citar-org-roam citar-embark cape)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
