415a32da0eb4a7ce01ae08b6acf51cda041365ee
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-04-27 11:32:52 -0500

    Accepted custom set variables stuff in emacs init.el

--- emacs/init.el
+++ emacs/init.el
@@ -657,3 +657,16 @@
 ;;   (if (eq major-mode 'pdf-view-mode)
 ;;       (org-noter-sync-current-note)
 ;;     (org-noter)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-preview-latex-default-process 'dvisvgm nil nil "Customized with use-package org")
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
