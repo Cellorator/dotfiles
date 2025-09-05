21a040917a2153e9ad222ca92eabcd7cf637d08d
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-09-05 18:48:25 -0500

    Removed android specific emacs configuration

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,6 +1,1 @@
-(when (eq system-type 'android)
-  (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
-			 (getenv "PATH")))
-  (push "/data/data/com.termux/files/usr/bin" exec-path))
-
 (setenv "LSP_USE_PLISTS" "true")
