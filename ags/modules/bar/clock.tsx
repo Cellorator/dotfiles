0407fdab2f8851c5b3212a8cb0d716db20fb2ab2
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-12-25 17:21:09 -0600

    ags: Fixed clock widget to actually poll properly

--- ags/modules/bar/clock.tsx
+++ ags/modules/bar/clock.tsx
@@ -2,14 +2,12 @@ import { createPoll } from "ags/time"
 import GLib from "gi://GLib"
 import { BackwardSlashSeparator } from "../widgets/separator.js"
 
-const d = GLib.DateTime.new_now_local()
-const day = createPoll("", 1000, () => d.format("%A"))
-const date = createPoll("", 1000, () => d.format("%b %d"))
-const time = createPoll("", 1000, () => d.format("%H:%M"))
+const day = createPoll("", 1000, () => GLib.DateTime.new_now_local().format("%A"))
+const date = createPoll("", 1000, () => GLib.DateTime.new_now_local().format("%b %d"))
+const time = createPoll("", 1000, () => GLib.DateTime.new_now_local().format("%H:%M"))
 
 export default function Clock() {
-  return <box
-           class='clock'>
+  return <box class='clock'>
            <label class='day' label={day} />
            <BackwardSlashSeparator />
            <label class='date' label={date} />
