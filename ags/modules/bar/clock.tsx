f5504e23d53b2654babd40945d6a7902247bcf8a
Cellorator <66387348+Cellorator@users.noreply.github.com>
2025-12-25 00:52:04 -0600

    ags: Updated clock module to use new ags stuff

--- ags/modules/bar/clock.tsx
+++ ags/modules/bar/clock.tsx
@@ -1,17 +1,17 @@
-import { Variable } from "astal"
+import { createPoll } from "ags/time"
 import { BackwardSlashSeparator } from "../widgets/separator.js"
 
-const day = Variable("").poll(1000, 'date "+%A"')
-const date = Variable("").poll(1000, 'date "+%b %d"')
-const time = Variable("").poll(1000, 'date "+%H:%M"')
+const day = createPoll("", 1000, 'date "+%A"')
+const date = createPoll("", 1000, 'date "+%b %d"')
+const time = createPoll("", 1000, 'date "+%H:%M"')
 
 export default function Clock() {
   return <box
-           className='clock'>
-           <label className='day' label={day()} />
+           class='clock'>
+           <label class='day' label={day} />
            <BackwardSlashSeparator />
-           <label className='date' label={date()} />
+           <label class='date' label={date} />
            <BackwardSlashSeparator />
-           <label className='time' label={time()} />
+           <label class='time' label={time} />
          </box>
 }
